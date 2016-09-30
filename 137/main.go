42f5dc82759dce481aff426091186b4bd17a763b
KoanJan <j.kehong@gmail.com>
2016-09-30 11:24:28 +0800

    feat: 137. Single Number II

--- 137/main.go
+++ 137/main.go
@@ -10,11 +10,13 @@ func singleNumber(nums []int) int {
 	}
 	var (
 		res int = 0
-		cur int = 1
 	)
 	// 32-bit
 	for i := 0; i < 32; i++ {
-		times := 0
+		var (
+			times int = 0
+			cur       = 1 << uint(i)
+		)
 		for _, n := range nums {
 			if n&cur > 0 {
 				times++
@@ -24,14 +26,15 @@ func singleNumber(nums []int) int {
 		if times%3 == 1 {
 			res |= cur
 		}
-		cur = 1 << uint(i+1)
 	}
-	return res
+	// int is 32 or 64 bit, which depends on OS
+	return int(int32(res))
 }
 
 func main() {
 
 	// test
 	nums := []int{-2, -2, 1, 1, -3, 1, -3, -3, -4, -2}
+	// nums := []int{1, 1, 1, 4}
 	fmt.Printf("In %v, single num is %d\n", nums, singleNumber(nums))
 }
