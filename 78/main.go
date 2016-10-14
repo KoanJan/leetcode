cb4a006057aa1d51c427410221b15e64f97f181d
KoanJan <j.kehong@gmail.com>
2016-10-14 11:08:17 +0800

    feat: 78. Subsets

--- 78/main.go
+++ 78/main.go
@@ -5,19 +5,42 @@ import (
 )
 
 func subsets(nums []int) [][]int {
-	return genSets([]int{}, nums)
+	res := genSets([]int{}, nums)
+	for i := 0; i < len(res); i++ {
+		fuckingSort(res[i])
+	}
+	return res
 }
 
 func genSets(pre []int, nums []int) [][]int {
 	res := [][]int{pre}
 	for i := 0; i < len(nums); i++ {
-		newPre := append(pre, nums[i])
-		sets := genSets(newPre, nums[i+1:])
+		sets := genSets(append(cpSlice(pre), nums[i]), nums[0:i])
 		for _, r := range sets {
 			res = append(res, r)
 		}
 	}
 	return res
+}
+
+func cpSlice(src []int) []int {
+	res := []int{}
+	for i := 0; i < len(src); i++ {
+		res = append(res, src[i])
+	}
+	return res
+}
+
+func fuckingSort(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := len(nums) - 1; j > i; j-- {
+			if nums[j] < nums[i] {
+				tmp := nums[i]
+				nums[i] = nums[j]
+				nums[j] = tmp
+			}
+		}
+	}
 }
 
 func subSetString(set [][]int) string {
@@ -33,6 +56,6 @@ func subSetString(set [][]int) string {
 
 func main() {
 	// test
-	nums := []int{1, 2, 3}
+	nums := []int{9, 0, 3, 5, 7}
 	fmt.Printf("subsets of %v:\n%s", nums, subSetString(subsets(nums)))
 }
