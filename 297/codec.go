7508694bc7a296ca2c0462d5329c32ce79502184
KoanJan <j.kehong@gmail.com>
2016-11-21 16:26:38 +0800

    feat: 297. Serialize and Deserialize Binary Tree

--- 297/codec.go
+++ 297/codec.go
@@ -57,10 +57,10 @@ func serialize(root ...*TreeNode) string {
 		return ""
 	}
 	var (
-		res  string      = fmt.Sprintf("%d", root[0].Val)
+		res  string
 		next []*TreeNode = []*TreeNode{}
 	)
-	for i := 1; i < len(root); i++ {
+	for i := 0; i < len(root); i++ {
 		if root[i] == nil {
 			res += ",null"
 		} else {
@@ -68,6 +68,7 @@ func serialize(root ...*TreeNode) string {
 			next = append(next, root[i].Left, root[i].Right)
 		}
 	}
+	res = string([]byte(res)[1:])
 	if len(next) > 0 {
 		res += "," + serialize(next...)
 	}
@@ -135,3 +136,95 @@ func deserialize(data []string, root ...*TreeNode) {
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.deserialize(codec.serialize(root));
+
+//////////////////////
+// AC-ed JAVA code  //
+//////////////////////
+
+// public class Codec {
+//     // Encodes a tree to a single string.
+//     public String serialize(TreeNode root) {
+//         //
+//         List<TreeNode> nodes = new ArrayList<TreeNode>();
+//         nodes.add(root);
+//         return "[" + s(nodes).substring(1) + "]";
+//     }
+
+//     private String s(List<TreeNode> nodes){
+//         //
+//         String res = ",";
+//         if (nodes.isEmpty()) {
+//             return res;
+//         }
+//         List<TreeNode> next = new ArrayList<TreeNode>();
+//         Iterator<TreeNode> iter = nodes.iterator();
+//         while (iter.hasNext()) {
+//             TreeNode node = iter.next();
+//             if (node == null) {
+//                 res += ",null";
+//             }else{
+//                 res += "," + String.valueOf(node.val);
+//                 next.add(node.left);
+//                 next.add(node.right);
+//             }
+//         }
+//         res = res.substring(1);
+//         if (!next.isEmpty()) {
+//             res += s(next);
+//         }
+//         return res;
+//     }
+
+//     // Decodes your encoded data to tree.
+//     public TreeNode deserialize(String data) {
+//         //
+//         if (data.length() < 2) {
+//             return null;
+//         }
+//         String[] dataArray = data.substring(1, data.length()-1).split(",");
+//         if (dataArray.length == 0) {
+//             return null;
+//         }
+//         if (dataArray[0].equals("null")){
+//             return null;
+//         }
+//         List<String> dataList = Arrays.asList(dataArray);
+//         TreeNode root = new TreeNode(0);
+//         Iterator<String> iter = dataList.iterator();
+//         if (iter.hasNext()) {
+//             root.val = Integer.parseInt(iter.next());
+//         }
+//         List<TreeNode> nodes = new ArrayList();
+//         nodes.add(root);
+//         d(dataList.subList(1,dataList.size()), nodes);
+//         return root;
+//     }
+
+//     private void d(List<String> dataList, List<TreeNode> nodes){
+//         //
+//         if (dataList.isEmpty() || nodes.isEmpty()) {
+//             return;
+//         }
+//         //
+//         List<TreeNode> next = new ArrayList<TreeNode>();
+//         int i = 0;
+//         for (i = 0; i < nodes.size(); i++){
+//             int j = 2*i;
+//             if (dataList.get(j).equals("null")){
+//                 nodes.get(i).left = null;
+//             }else{
+//                 nodes.get(i).left = new TreeNode(Integer.parseInt(dataList.get(j)));
+//                 next.add(nodes.get(i).left);
+//             }
+//             if (dataList.get(j+1).equals("null")){
+//                 nodes.get(i).right = null;
+//             }else{
+//                 nodes.get(i).right = new TreeNode(Integer.parseInt(dataList.get(j+1)));
+//                 next.add(nodes.get(i).right);
+//             }
+//         }
+//         if (!next.isEmpty()){
+//             d(dataList.subList(2*i,dataList.size()),next);
+//         }
+//     }
+// }
